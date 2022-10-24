a6917e3399e45e55c0cd3d20200801d4a469c8ec
Rxann <80224521+Rxann@users.noreply.github.com>
2022-10-23 20:46:16 -0400

    Finish a version of the tele function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const os = require("os");
-const axios = require("axios");
+const axios = require("axios").default;
+// server runs default port: 2525
 
-function tele(serveraddr) {}
+function tele(serveraddr) {
+  axios({
+    method: "post",
+    url: `${serveraddr}`,
+    data: {
+      loadavg: os.loadavg(),
+      totalmem: os.totalmem(),
+      os: os.type(),
+      arch: os.arch(),
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return new Error(
+        `Lima Telemetry Project Error! Post request to provided Lima server has failed! Provided error:${err}`
+      );
+    });
+}
